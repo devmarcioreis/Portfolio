8b75c16a4de925bf0131d05b8ee8c8671380768f
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-09-22 19:02:38 -0300

    Inicio desenvolvimento novo portfólio

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+//Menu
+const open = document.querySelector('#open-toggle');
+const close = document.querySelector('#close-toggle')
+const nav = document.querySelector('#nav-menu')
+const navLink = document.querySelectorAll('.nav__link');
+
+const menu = () => {
+    if(open && nav) {
+        open.addEventListener('click', () => {
+            nav.classList.toggle('show')
+            open.style.display = 'none'
+            close.style.display = 'inline-block'
+        })
+    }
+    
+    if(close && nav) {
+        close.addEventListener('click', () => {
+            nav.classList.remove('show')
+            close.style.display = 'none'
+            open.style.display = 'inline-block'
+        })
+    }
+
+};
+menu()
+
+const linkAction = () => {
+    nav.classList.remove('show')
+    close.style.display = 'none'
+    open.style.display = 'inline-block'
+}
+navLink.forEach(headerNav => headerNav.addEventListener('click', linkAction));
+
+
+//Navbar color
+window.onscroll = () => {
+    const nav = document.getElementById('header');
+
+    if(this.scrollY >= 100) nav.classList.add('scroll-header');
+    else nav.classList.remove('scroll-header');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
